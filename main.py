19324093bd418236cebaa860ffb9bfb85092699b
FxcuEsq <facuesquivel338@gmail.com>
2026-09-03 21:47:35 -0300

    feat: estructura base del proyecto del sistema

---
+++ main.py
@@ -0,0 +1,5 @@
+def registrar_asistencia(dni: str):
+    print(f"Boceto inicial: procesando DNI {dni}...")
+if __name__ == "__main__":
+    print("=== SISTEMA DE ASISTENCIA v0.1.0 (Dev) ===")
+registrar_asistencia("40123456")
